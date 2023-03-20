ded642145480c6eee9ff3aebc4e12db177baa273
Mateus Barbosa <mateuscbarbosa@gmail.com>
2023-03-20 17:25:26 -0300

    Projeto Mochila de Viagem com LocalStorage

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    criarElemento(elemento)
+});
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    
+    const nome = evento.target.elements['nome']
+    const quantidade =evento.target.elements['quantidade']
+    
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    criarElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
+})
+
+function criarElemento(item){
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += item.nome
+
+    lista.appendChild(novoItem)
+}
