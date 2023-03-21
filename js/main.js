9bce75df86bb38c55402e1391fad0ae88f499cbb
Mateus Barbosa <mateuscbarbosa@gmail.com>
2023-03-21 12:27:13 -0300

    Remoção de itens

--- js/main.js
+++ js/main.js
@@ -23,10 +23,10 @@ form.addEventListener("submit", (evento) => {
         itemAtual.id = existe.id
         atualizaElemento(itemAtual)
 
-        itens[existe.id] = itemAtual
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     }
     else{
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id +1 : 0;
 
         criarElemento(itemAtual)
     
@@ -50,9 +50,30 @@ function criarElemento(item){
     novoItem.appendChild(numeroItem)
     novoItem.innerHTML += item.nome
 
+    novoItem.appendChild(botaoDeleta(item.id))
+
     lista.appendChild(novoItem)
 }
 
 function atualizaElemento(item){
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id){
+    tag.remove()
+    
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
